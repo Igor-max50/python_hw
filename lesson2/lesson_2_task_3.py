740aae466e12bd8dbe4878bb1dd9b35f60455d8c
Igor <konstantin30.05@mail.ru>
2025-09-21 17:47:00 +0300

    Delete lesson2 directory

--- lesson2/lesson_2_task_3.py
+++
@@ -1,9 +0,0 @@
-# Вычисление площади квадрата по одной стороне
-def square(sq):
-    if (sq > 0):
-        return True
-    else:
-        return False
-sq = int(input("Введите длинну стороны квадрата: "))
-size = sq * sq
-print(f"Площадь квадрата со стороной {sq} = {size}")
